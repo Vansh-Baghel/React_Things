f546c185d5df209bb3f771d1d5bb0c867724697b
Vansh Baghel <103327712+Vansh-Baghel@users.noreply.github.com>
2022-09-05 08:36:31 -0700

    Delete src directory

--- src/Components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import "../Expenses-Folder/ExpenseStyle.css";
-
-function Card(props) {
-  const classes = 'card ' + props.className;
-  return <div className={classes}>{props.children}</div>;
-}
-
-export default Card;
